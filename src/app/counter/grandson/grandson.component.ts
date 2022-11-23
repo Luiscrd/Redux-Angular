6f8943ae341ef7b63b07e7b3d9a514f04572e4eb
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-23 08:18:44 +0100

    upload

--- src/app/counter/grandson/grandson.component.ts
+++ src/app/counter/grandson/grandson.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-grandson',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GrandsonComponent implements OnInit {
 
+  @Input() counter: number = 0;
+
+  @Output() changeCounter = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  reset(number: number){
+    this.counter = number;
+    this.changeCounter.emit(number);
   }
 
 }
