b9fb36457ae2fc5818a52a4017aaa08a21fd2991
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-23 12:18:26 +0100

    update

--- src/app/counter/grandson-redux/grandson-redux.component.ts
+++ src/app/counter/grandson-redux/grandson-redux.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import { reset } from '../counter.actions';
